bc3761c3bdf7dbdb74988dd20ff2532b52e5b8f3
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-12-07 23:49:07 -0700

    aaa

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,49 @@
 (function ($) {
-
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
+  // PRE LOADER
+  $(window).load(function () {
+    $(".preloader").fadeOut(1000); // set duration in brackets
+    $(".read-more").click(function (e) {
+      e.preventDefault();
+      let id = $(this).data("id");
+      $(this).closest(".content-customer-short").hide();
+      $("#content-customer-full-" + id).show();
     });
+  });
 
+  //Navigation Section
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-    //Navigation Section
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
+  // Owl Carousel
+  $(".owl-carousel").owlCarousel({
+    animateOut: "fadeOut",
+    items: 1,
+    loop: true,
+    autoplay: true,
+  });
+
+  // PARALLAX EFFECT
+  $.stellar();
+
+  // SMOOTHSCROLL
+  $(function () {
+    $(".navbar-default a, #home a, footer a").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
     });
+  });
 
-
-    // Owl Carousel
-    $('.owl-carousel').owlCarousel({
-      animateOut: 'fadeOut',
-      items:1,
-      loop:true,
-      autoplay:true,
-    })
-
-
-    // PARALLAX EFFECT
-    $.stellar();  
-
-
-    // SMOOTHSCROLL
-    $(function() {
-      $('.navbar-default a, #home a, footer a').on('click', function(event) {
-        var $anchor = $(this);
-          $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-          }, 1000);
-            event.preventDefault();
-      });
-    });  
-
-
-    // WOW ANIMATION
-    new WOW({ mobile: false }).init();
-
+  // WOW ANIMATION
+  new WOW({ mobile: false }).init();
 })(jQuery);
